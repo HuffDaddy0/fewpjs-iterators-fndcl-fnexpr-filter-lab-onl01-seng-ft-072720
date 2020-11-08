68dfb4b3698d73285c13bdfd73da938b45ed77be
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-08 17:56:53 -0500

    commit tree 4d0b70ac2f1f35be53fe10d62bd675a7c41b7d41

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, stringInput){
+    const solution = array.filter(function(string, stringInput){
+        const standardString = stringInput.split("").map(letter => letter.toLowerCase())
+        debugger
+        return string === standardString
+    })
+    return solution
+};
+
+function fuzzyMatch(array, string){
+    array.find(function(index){
+        return index.charAt(0) === string.charAt(0)
+    });
+};
+
